76144761b81c3b85cfff0dc215b5b9f480640852
aydinmu17 <44281343+aydinmu17@users.noreply.github.com>
2020-11-25 05:08:52 +0300

    developed login system

--- settings.py
+++ settings.py
@@ -1,1 +1,11 @@
 DEBUG = True
+SECRET_KEY = "secret"
+WTF_CSRF_ENABLED = True
+PASSWORDS = {
+    "admin": "$pbkdf2-sha256$29000$PIdwDqH03hvjXAuhlLL2Pg$B1K8TX6Efq3GzvKlxDKIk4T7yJzIIzsuSegjZ6hAKLk",
+    "cordi": "$pbkdf2-sha256$29000$fk9J6f0fQ8hZSykFwJjT2g$kvI6dSWvHo6y1XMb9Nj8/tuykxTrbU8oTSY7W8Fbyyc",
+    "normaluser":"$pbkdf2-sha256$29000$g5CS8n7PeY9xDmGslbJW6g$2sDoK21bAwKtxmQsgXpMS2hRJfeCRDdQJ/j6CttpqDE",
+}
+
+ADMIN_USERS = ["admin"]
+COORDINATORS = ["cordi"]
