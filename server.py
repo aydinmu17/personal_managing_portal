77bc08c8866fb46ec8c8fe9f88624df9b8bbe065
aydinmu17 <44281343+aydinmu17@users.noreply.github.com>
2020-11-25 05:07:54 +0300

    developed login system

--- server.py
+++ server.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, abort, session
 from datetime import datetime
 import view
 from database import Database
 from task import Task
+from flask_login import UserMixin, LoginManager
+from users import get_user
+
+login_manager = LoginManager()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return get_user(user_id)
 
 
 def crate_app():
     app = Flask(__name__)
+    app.add_url_rule("/login", view_func=view.login_page, methods=["GET", "POST"])
+    app.add_url_rule("/logout", view_func=view.logout_page, methods=["GET", "POST"])
     app.add_url_rule("/tasks", view_func=view.tasks_page)
     app.add_url_rule("/tasks/<int:task_key>", view_func=view.task_page)
     app.add_url_rule("/", view_func=view.main_page)
@@ -16,15 +27,16 @@ def crate_app():
     app.add_url_rule("/task-edit/<int:task_key>", view_func=view.edit_task_page_master, methods=["GET", "POST"])
 
     db = Database()
-    for x in range(1,11):
-        db.add_task(Task("Task "+ str(x), url="task_"+str(x)))
+    for x in range(1, 11):
+        db.add_task(Task("Task " + str(x), url="task_" + str(x)))
 
     db.add_task(Task("Bu bir tasktir ", url="task_" + str(x)))
-
     app.config["db"] = db
 
+    login_manager.init_app(app)
+    login_manager.login_view = "login_page"
+
     app.config.from_object("settings")
-    # app.config["DEBUG"] = True
     return app
 
 
