4a7029bd37652ed3ae951f6e54cafbb43e3c8c16
aydinmu17 <44281343+aydinmu17@users.noreply.github.com>
2021-01-20 05:52:23 +0300

    Delete database.py

--- database.py
+++
@@ -1,4 +0,0 @@
-TaskManager.add_task(Task("Enroll Project", "enroll_project","employee"))
-TaskManager.add_task(Task("My profile", "my_profile","employee"))
-TaskManager.add_task(Task("My Team", "my_teams","employee"))
-TaskManager.add_task(Task("My projects", "my_projects","employee"))
